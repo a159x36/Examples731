1b34ba53bd57090d825dcb8cfab17073e65af9bb
Martin Johnson <M.J.Johnson@massey.ac.nz>
2024-05-07 12:34:33 +1200

    Add Fast Walsh Hadamard Transform

--- Lecture9/fft/src/main.cpp
+++ Lecture9/fft/src/main.cpp
@@ -12,18 +12,95 @@ using namespace cv;
 using namespace std;
 using namespace chrono;
 
+const int MAXFWHT_SIZE=1024;
+
+// reverse the bits in n
+int bit_reverse( int n, unsigned nbits ) {
+    int r=0;
+    for(;nbits--;n=n>>1) { 
+        r=r<<1 | (n&1);
+    }
+    return r;
+}
+
+// work out sequency_permutation for fwht
+void fwht_sequency_permutation(int perm[], unsigned order ) {
+	int size=1<<order;
+	for ( int i=0;i<size;i++) 
+        perm[i] = bit_reverse((i >> 1) ^ i,order);
+}
+
+// A 1D Fast Walsh Hadamard Transform, n must be  power of 2
+// l2n is log base 2 n, perm is an array of sequency permutations
+void fwht1d(float *data, const int n, int l2n, int stride, int *perm, int scale) {
+    float copy[MAXFWHT_SIZE];
+    for(int i=0;i<n;i++) {
+        copy[i]=data[i*stride];
+    }
+	for (int i = 0; i < l2n; ++i ) {
+        int o=(1<<i);
+		for (int j = 0; j < n; j += o*2) {
+            for (int k = 0; k < o; ++k ) {
+                float t=copy[j+k];
+                float t1=copy[j+k+o];
+                copy[j+k]=t+t1;
+                copy[j+k+o]=t-t1;
+            }
+        }
+	}
+    for(int i=0;i<n;i++) {
+        data[i*stride]=copy[perm[i]]/scale;
+    }
+}
+// returns log base 2 the smallest power of 2 >= x 
+int log2(int x) {
+    int i=1;
+    int n=0;
+    while(i<x) {
+        i=i<<1;
+        n++;
+    }
+    return n;
+}
+
+// a 2d Fast Walsh Hadamard Transform
+void fwht(Mat_<float> in, bool forward) {
+    int ln2c=log2(in.cols);
+    int ln2r=log2(in.rows);
+    int cols=1<<ln2c;
+    int rows=1<<ln2r;
+    Mat_<float> out;
+    copyMakeBorder(in, out, 0, rows-in.rows, 0, cols-in.cols, BORDER_CONSTANT,0);
+    int perm[MAXFWHT_SIZE];
+    fwht_sequency_permutation(perm,ln2c);
+    for(int i=0;i<out.rows;i++) {
+        fwht1d(&out(i,0), out.cols, ln2c, 1,perm, forward?out.cols:1);
+    }
+    fwht_sequency_permutation(perm,ln2r);
+    for(int i=0;i<out.cols;i++) {
+        fwht1d(&out(0,i), out.rows, ln2r, out.cols ,perm, forward?out.rows:1);
+    }
+    for(int i=0;i<in.rows;i++) 
+        for(int j=0;j<in.cols;j++)
+            in(i,j)=out(i,j); 
+   // return in;
+}
 // main, uses webcam to get a colour image, encoded it using the dct, quantizes this
 // then dequantizes it and decodes it using an idct. 
 int main(int argc, char** argv) {
     bool COLOUR=true;
     bool WEBCAM=true;
     bool WEINER=true;
+    bool HADAMARD=false;
+
+ //   if(HADAMARD) WEBCAM=false;
 
     (void)argv[argc - 1];
     VideoCapture cap;
     Mat_<Vec3b> frame;
     Mat_<Vec3b> image;
     image=imread("../../../../Images/clown.tif");
+   // image=imread("../../../../Images/baboon.jpg");
     frame=image.clone();
     if(WEBCAM) {
 #if __linux__
@@ -39,12 +116,12 @@ int main(int argc, char** argv) {
         //   cap.set(CAP_PROP_FRAME_WIDTH, 960);
         //   cap.set(CAP_PROP_FRAME_WIDTH, 1600);
         //   cap.set(CAP_PROP_FRAME_HEIGHT, 480);
-        //   cap.set(CAP_PROP_FRAME_HEIGHT, 720);
+       //    cap.set(CAP_PROP_FRAME_HEIGHT, 720);
         //   cap.set(CAP_PROP_FRAME_HEIGHT, 1080);
         cap >> frame;
     }
     
-    Menu m("Deconvolution",{"Weiner Filter","Pseudoinverse","Colour","Greyscale","Webcam","Static Image","Exit"});
+    Menu m("Deconvolution",{"Weiner Filter","Pseudoinverse","Colour","Greyscale","Webcam","Static Image","FWHT","FFT","Exit"});
     namedWindow("WebCam", WINDOW_FREERATIO);
     namedWindow("Result", WINDOW_FREERATIO);
 
@@ -87,6 +164,28 @@ int main(int argc, char** argv) {
         
         int nchannels=1;
         if(COLOUR) nchannels=3;
+
+        if(HADAMARD) {
+            Mat_<float> hadamard(frame.size());
+            for(int ch=0;ch<nchannels;ch++) {
+            channels[ch].convertTo(hadamard,CV_32FC1);
+            float rad=(cutoff/100.0)/5;
+            fwht(hadamard,true);
+            for(int i=0;i<hadamard.rows;i++) {
+                for(int j=0;j<hadamard.cols;j++) {
+                    float d;
+                    //d=rad*min(i,j)/10;
+                    d=rad*(hypot(hadamard.rows,hadamard.cols)-hypot(hadamard.rows-i,hadamard.cols-j))/100;
+                    float G;
+                    G=exp(-d*d);
+                    hadamard(i,j)*=G;
+                }
+            }
+            imshow("Hadamard",hadamard+0.5);
+            fwht(hadamard,false);
+            hadamard.convertTo(channels[ch],CV_8UC1);
+            }
+        } else 
 
         for(int ch=0;ch<nchannels;ch++) {
             planes[0]=Mat_<float>(channels[ch].clone());
@@ -162,6 +261,12 @@ int main(int argc, char** argv) {
         }
         if(entry=="Static Image") {
             WEBCAM=false;
+        }
+        if(entry=="FWHT") {
+            HADAMARD=true;
+        }
+        if(entry=="FFT") {
+            HADAMARD=false;
         }
         system_clock::time_point end = system_clock::now();
         float mseconds = (end - start) / 1ms;
